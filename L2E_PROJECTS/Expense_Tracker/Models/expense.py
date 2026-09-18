199569e6a0ba5cd098537baefe13d1fd88c34f4d
kmrital <olubukola11th@gmail.com>
2026-09-18 16:08:01 +0100

    intialize expense tracker project

---
+++ L2E_PROJECTS/Expense_Tracker/Models/expense.py
@@ -0,0 +1,6 @@
+class Expense:
+    def __init__(self, category, amount, description, createdAt):
+        self.category = category
+        self.amount = amount
+        self.description = description
+        self.dateCreated = createdAt
